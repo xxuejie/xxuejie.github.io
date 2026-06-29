f5e6cfb2577211243cae63aab317725ed047fb58
Xuejie Xiao <xxuejie@gmail.com>
2026-06-29 05:35:23 +0000

    Update commit script to cleanup public folder first

--- commit-gh-pages-files.sh
+++ commit-gh-pages-files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Clean all staged, unstaged, tracked files first
+cd public && git reset --hard HEAD && git clean -fdx && cd..
+
 hugo && cd public && git add --all && git commit -m "Publishing to master" && cd ..
